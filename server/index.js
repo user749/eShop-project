a8d09e59f92dc69cfc2435f7ce38d7ae0c2f8469
user749 <lolyou331@yandex.com>
2023-04-02 13:29:22 +0200

    Backend schema completed

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+
+import connectToDatabase from "./database.js"
+import  express  from "express"
+
+//Our routes
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+connectToDatabase();
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/products", productRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}.`)
+});
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import Product from "../models/Products.js";
+const productRoutes = express.Router();
+
+
+
+const getProducts = async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+}
+
+productRoutes.route("/").get(getProducts);
+
+export default productRoutes;
