98c1176febeefa0dace2fa4eff78e2d4fecd642d
user749 <lolyou331@yandex.com>
2023-04-06 21:40:42 +0200

    product screen

--- client/src/redux/slices/products.js
+++ client/src/redux/slices/products.js
@@ -1,32 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const initialState = {
-    loading:false,
-    error: null,
-    products: []
-}
+  loading: false,
+  error: null,
+  products: [],
+  product: null,
+};
 
 export const productsSlice = createSlice({
-
-    name: "products",
-    initialState,
-    reducers: {
-        setLoading: (state) => {
-            state.loading = true;
-        },
-        setProducts: (state, {payload}) => {
-            state.loading = false;
-            state.error = null;
-            state.products = payload;
-        },
-        setError: (state, {payload}) => {
-            state.error = payload;
-            state.loading = false;
-        },
+  name: "products",
+  initialState,
+  reducers: {
+    setLoading: (state) => {
+      state.loading = true;
     },
-})
+    setProducts: (state, { payload }) => {
+      state.loading = false;
+      state.error = null;
+      state.products = payload;
+    },
+    setProduct: (state, { payload }) => {
+      state.product = payload;
+      state.loading = false;
+      state.error = null;
+    },
+    setError: (state, { payload }) => {
+      state.error = payload;
+      state.loading = false;
+    },
+  },
+});
 
-export const {setLoading, setError, setProducts} = productsSlice.actions;
+export const { setLoading, setError, setProducts, setProduct } =
+  productsSlice.actions;
 export default productsSlice.reducer;
 
 export const productsSelector = (state) => state.products;
